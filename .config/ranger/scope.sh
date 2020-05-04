a71e2a0e24bfacd3a18b6bd341bec9f48d590825
Guillaume Levasseur <phenyl@posteo.net>
2020-05-04 14:40:55 +0200

    feat: ranger: add preview for CSV and JSON files

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -64,7 +64,7 @@ case "$extension" in
         try transmission-show "$path" && { dump | trim; exit 5; } || exit 1;;
     # ODT Files
     odt|ods|odp|sxw)
-	try odt2txt "$path" && { dump | trim; exit 5; } || exit 1;;    
+    try odt2txt "$path" && { dump | trim; exit 5; } || exit 1;;
     # HTML Pages:
     htm|html|xhtml)
         try w3m    -dump "$path" && { dump | trim | fmt -s -w $width; exit 4; }
@@ -75,7 +75,7 @@ esac
 
 case "$mimetype" in
     # Syntax highlight for text files:
-    text/* | */xml)
+    text/* | */xml | */csv | */json)
         try highlight --out-format=ansi "$path" && { dump | trim; exit 5; } || exit 2;;
     # image preview:
     #    cp "$path" "$cached" && exit 6 || exit 1;;
